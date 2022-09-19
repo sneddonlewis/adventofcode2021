bc80ad2268da670c4e022ab719fe36d25b9859f8
Lewis Sneddon <lewis@lewiscodes.io>
2022-09-19 10:29:00 +0100

    map return type mismatch when flattening'

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -4,6 +4,54 @@ fn main() {
     println!("{:?}", get_input());
 }
 
+// get gamma rate
+// get epsilon rate
+// multiply them together
+
+fn compute_gamma(reading: &str) -> &'static str {
+    let result = reading.lines().map(more_ones);
+    println!("{:?} debugging compute_gamma", result);
+    "10110"
+}
+
+#[test]
+fn compute_gamma_test_22() {
+    let test_input = get_input();
+    let result = compute_gamma(test_input);
+    let expected = "10110";
+
+    assert_eq!(expected, result);
+}
+
+fn more_ones(reading_pos: Vec<i32>) -> &'static str {
+    let mut count = 0;
+
+    for i in reading_pos.iter() {
+        if *i == 1 {
+            count += 1;
+        }
+    }
+
+    if reading_pos.len() / 2 < count {
+        "1"
+    } else {
+        "0"
+    }
+}
+
+#[test]
+fn more_ones_test_one() {
+    let one = vec![1, 1, 0, 1];
+    assert_eq!("1", more_ones(one));
+}
+
+#[test]
+fn more_ones_test_zero() {
+    let zero = vec![1, 0, 0, 0];
+
+    assert_eq!("0", more_ones(zero));
+}
+
 fn get_input() -> &'static str {
     "00100
 11110
